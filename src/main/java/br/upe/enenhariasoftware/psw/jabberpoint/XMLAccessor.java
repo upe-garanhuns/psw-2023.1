c1fc9cd1793a22338e3c92211f3c693486f953c4
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2023-07-14 18:18:20 -0300

    Refatorando XMLAccessor: Reformatação dos imports

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/XMLAccessor.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/XMLAccessor.java
@@ -14,11 +14,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Vector;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
