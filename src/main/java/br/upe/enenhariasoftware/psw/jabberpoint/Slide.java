f58df314d65e0a17d1428d7f77ed90472ab263bd
alina <alccosta03@gmail.com>
2023-07-15 11:40:32 -0300

    mudança de ordem dos modificadores

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/Slide.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/Slide.java
@@ -16,8 +16,8 @@ import java.util.Vector;
 
 public class Slide {
 
-	public final static int WIDTH = 1200;
-	public final static int HEIGHT = 800;
+	public static final int WIDTH = 1200;
+	public static final int HEIGHT = 800;
 
 	protected TextItem title;
 	protected Vector<SlideItem> items;
