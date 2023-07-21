ecd691365a3679be8abdbd51d29294e50c719bae
unknown <mateusva.1132@gmail.com>
2023-07-21 19:33:20 -0300

    Refatoração da classe TextItem

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/TextItem.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/TextItem.java
@@ -54,7 +54,8 @@ public class TextItem extends SlideItem {
 	public Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style myStyle) {
 		List<TextLayout> layouts = getLayouts(g, myStyle, scale);
 
-		int xsize = 0, ysize = (int) (myStyle.leading * scale);
+		int xsize = 0;
+		int ysize = (int) (myStyle.leading * scale);
 
 		Iterator<TextLayout> iterator = layouts.iterator();
 
@@ -100,7 +101,7 @@ public class TextItem extends SlideItem {
 	}
 
 	private List<TextLayout> getLayouts(Graphics g, Style s, float scale) {
-		List<TextLayout> layouts = new ArrayList<TextLayout>();
+		List<TextLayout> layouts = new ArrayList<>();
 
 		AttributedString attrStr = getAttributedString(s, scale);
 		Graphics2D g2d = (Graphics2D) g;
