f8b4e6c9c0964a4937ab87ba438c932d141a90b7
Gabriel Ferreira da Silva <126522573+GabrielFerreiraSilva@users.noreply.github.com>
2023-07-20 21:20:27 -0300

    Refatoração das classes SlideViewerComponent e Acessor

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerComponent.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerComponent.java
@@ -20,23 +20,23 @@ import javax.swing.JFrame;
 public class SlideViewerComponent extends JComponent {
 
     private static final long serialVersionUID = 227L;
-    private static final Color BGCOLOR = Color.white;
+    private static final Color BACKGROUND_COLOR = Color.white;
     private static final Color COLOR = Color.black;
-    private static final String FONTNAME = "Dialog";
-    private static final int FONTSTYLE = Font.BOLD;
-    private static final int FONTHEIGHT = 10;
-    private static final int XPOS = 1100;
-    private static final int YPOS = 20;
+    private static final String FONT_NAME = "Dialog";
+    private static final int FONT_STYLE = Font.BOLD;
+    private static final int FONT_HEIGHT = 10;
+    private static final int X_POSITION = 1100;
+    private static final int Y_POSITION = 20;
 
     private Slide slide;
-    private Font labelFont = null;
-    private Presentation presentation = null;
-    private JFrame frame = null;
+    private final Font labelFont;
+    private Presentation presentation;
+    private final JFrame frame;
 
-    public SlideViewerComponent(Presentation pres, JFrame frame) {
-        setBackground(BGCOLOR);
-        presentation = pres;
-        labelFont = new Font(FONTNAME, FONTSTYLE, FONTHEIGHT);
+    public SlideViewerComponent(Presentation presentation, JFrame frame) {
+        setBackground(BACKGROUND_COLOR);
+        this.presentation = presentation;
+        this.labelFont = new Font(FONT_NAME, FONT_STYLE, FONT_HEIGHT);
         this.frame = frame;
     }
 
@@ -53,23 +53,22 @@ public class SlideViewerComponent extends JComponent {
         this.presentation = presentation;
         this.slide = data;
         repaint();
-        frame.setTitle(presentation.getTitle());
+        this.frame.setTitle(presentation.getTitle());
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-        g.setColor(BGCOLOR);
-        g.fillRect(0, 0, getSize().width, getSize().height);
+    public void paintComponent(Graphics graphics) {
+        graphics.setColor(BACKGROUND_COLOR);
+        graphics.fillRect(0, 0, getSize().width, getSize().height);
 
         if (presentation.getSlideNumber() < 0 || slide == null) {
             return;
         }
 
-        g.setFont(labelFont);
-        g.setColor(COLOR);
-        g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), XPOS, YPOS);
-        Rectangle area = new Rectangle(0, YPOS, getWidth(), (getHeight() - YPOS));
-        slide.draw(g, area, this);
+        graphics.setFont(labelFont);
+        graphics.setColor(COLOR);
+        graphics.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), X_POSITION, Y_POSITION);
+        Rectangle area = new Rectangle(0, Y_POSITION, getWidth(), (getHeight() - Y_POSITION));
+        slide.draw(graphics, area, this);
     }
-
 }
