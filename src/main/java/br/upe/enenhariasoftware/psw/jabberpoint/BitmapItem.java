52e560df2fee97b8fb501c1bc12c0890116d13cc
Thiago Barros <bunbismuth@proton.me>
2023-07-21 23:15:56 -0300

    Fixes in BitmapItem
    1. Safe delete do construtor da classe que não estava sendo utilizado.
    2. Adicionado annotation @Override nos métodos sobrescritos.
    3. (S106) System.err trocado por um Logger.

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/BitmapItem.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/BitmapItem.java
@@ -1,7 +1,7 @@
 /**
  * UPE - Campus Garanhuns Curso de Bacharelado em Engenharia de Software
  * Disciplina de Projeto de Software - 2023.1
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0
  * https://www.apache.org/licenses/LICENSE-2.0
  * 
@@ -15,14 +15,13 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
 public class BitmapItem extends SlideItem {
-
 	private BufferedImage bufferedImage;
-	private String imageName;
-
+	private final String imageName;
 	protected static final String FILE = "File ";
 	protected static final String NOTFOUND = " not found";
 
@@ -34,24 +33,21 @@ public class BitmapItem extends SlideItem {
 		try {
 			bufferedImage = ImageIO.read(new File(imageName));
 		} catch (IOException e) {
-			System.err.println(FILE + imageName + NOTFOUND);
+			Logger.getLogger(FILE + imageName + NOTFOUND);
 		}
-
-	}
-
-	public BitmapItem() {
-		this(0, null);
 	}
 
 	public String getName() {
 		return imageName;
 	}
 
+	@Override
 	public Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style myStyle) {
 		return new Rectangle((int) (myStyle.indent * scale), 0, (int) (bufferedImage.getWidth(observer) * scale),
 				((int) (myStyle.leading * scale)) + (int) (bufferedImage.getHeight(observer) * scale));
 	}
 
+	@Override
 	public void draw(int x, int y, float scale, Graphics g, Style myStyle, ImageObserver observer) {
 		int width = x + (int) (myStyle.indent * scale);
 		int height = y + (int) (myStyle.leading * scale);
@@ -60,6 +56,7 @@ public class BitmapItem extends SlideItem {
 				(int) (bufferedImage.getHeight(observer) * scale), observer);
 	}
 
+	@Override
 	public String toString() {
 		return "BitmapItem[" + getLevel() + "," + imageName + "]";
 	}
