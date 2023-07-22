50aba6ebd6bea7fabf2b359d9a794bab31206793
Thiago Barros <bunbismuth@proton.me>
2023-07-21 23:29:39 -0300

    Fixes in KeyController
    1. (S1161) Colocada a annotation @Override no método keyPressed();
    2. Switch case atualizada para uma enhanced switch para melhorar legibilidade e manutenibilidade.

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/KeyController.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/KeyController.java
@@ -1,7 +1,7 @@
 /**
  * UPE - Campus Garanhuns Curso de Bacharelado em Engenharia de Software
  * Disciplina de Projeto de Software - 2023.1
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0
  * https://www.apache.org/licenses/LICENSE-2.0
  * 
@@ -13,33 +13,20 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class KeyController extends KeyAdapter {
-
-	private Presentation presentation;
-
+	private final Presentation presentation;
 	public KeyController(Presentation p) {
 		presentation = p;
 	}
 
+	@Override
 	public void keyPressed(KeyEvent keyEvent) {
 		switch (keyEvent.getKeyCode()) {
-		case KeyEvent.VK_PAGE_DOWN:
-		case KeyEvent.VK_DOWN:
-		case KeyEvent.VK_ENTER:
-		case '+':
-			presentation.nextSlide();
-			break;
-		case KeyEvent.VK_PAGE_UP:
-		case KeyEvent.VK_UP:
-		case '-':
-			presentation.prevSlide();
-			break;
-		case 'q':
-		case 'Q':
-			System.exit(0);
-			break; // fix?
-		default:
-			break;
+			case KeyEvent.VK_PAGE_DOWN, KeyEvent.VK_DOWN, KeyEvent.VK_ENTER, '+' -> presentation.nextSlide();
+			case KeyEvent.VK_PAGE_UP, KeyEvent.VK_UP, '-' -> presentation.prevSlide();
+			case 'q', 'Q' -> System.exit(0);
+			default -> {
+				break;
+			}
 		}
 	}
-
 }
