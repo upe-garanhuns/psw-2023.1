7d4da3f17326e4a8a6a68d9c6d75c1b57a1d54f4
Lucas Camilo <lucas.camilobezerra@upe.br>
2023-07-22 14:47:47 -0300

    extração de atribuições de dentro de expressões na classe MenuController

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/MenuController.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/MenuController.java
@@ -55,7 +55,8 @@ public class MenuController extends MenuBar {
     MenuItem menuItem;
     
     Menu fileMenu = new Menu(FILE);
-    fileMenu.add(menuItem = mkMenuItem(OPEN));
+    menuItem = mkMenuItem(OPEN);
+    fileMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -73,7 +74,8 @@ public class MenuController extends MenuBar {
       }
     });
     
-    fileMenu.add(menuItem = mkMenuItem(NEW));
+    menuItem = mkMenuItem(NEW);
+    fileMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -82,7 +84,8 @@ public class MenuController extends MenuBar {
       }
     });
     
-    fileMenu.add(menuItem = mkMenuItem(SAVE));
+    menuItem = mkMenuItem(SAVE);
+    fileMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
@@ -97,7 +100,8 @@ public class MenuController extends MenuBar {
     
     fileMenu.addSeparator();
     
-    fileMenu.add(menuItem = mkMenuItem(EXIT));
+    menuItem = mkMenuItem(EXIT);
+    fileMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -108,7 +112,8 @@ public class MenuController extends MenuBar {
     add(fileMenu);
     
     Menu viewMenu = new Menu(VIEW);
-    viewMenu.add(menuItem = mkMenuItem(NEXT));
+    menuItem = mkMenuItem(NEXT);
+    viewMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -116,7 +121,8 @@ public class MenuController extends MenuBar {
       }
     });
     
-    viewMenu.add(menuItem = mkMenuItem(PREV));
+    menuItem = mkMenuItem(PREV);
+    viewMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -124,7 +130,8 @@ public class MenuController extends MenuBar {
       }
     });
     
-    viewMenu.add(menuItem = mkMenuItem(GOTO));
+    menuItem = mkMenuItem(GOTO);
+    viewMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
@@ -137,7 +144,8 @@ public class MenuController extends MenuBar {
     add(viewMenu);
     
     Menu helpMenu = new Menu(HELP);
-    helpMenu.add(menuItem = mkMenuItem(ABOUT));
+    menuItem = mkMenuItem(ABOUT);
+    helpMenu.add(menuItem);
     
     menuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent actionEvent) {
