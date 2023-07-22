07ca418b9c68bf231fe1b9206ab80c05a8a6b9ec
Ana Bia <123493745+beatrizldsantos@users.noreply.github.com>
2023-07-22 12:48:01 -0300

    Reordenação dos modificadores

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerFrame.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerFrame.java
@@ -20,8 +20,8 @@ public class SlideViewerFrame extends JFrame {
 
   private static final String JABTITLE = "Jabberpoint 1.6";
 
-  public final static int WIDTH = 1200;
-  public final static int HEIGHT = 800;
+  public static final  int WIDTH = 1200;
+  public static final  int HEIGHT = 800;
 
   public SlideViewerFrame(String title, Presentation presentation) {
     super(title);
