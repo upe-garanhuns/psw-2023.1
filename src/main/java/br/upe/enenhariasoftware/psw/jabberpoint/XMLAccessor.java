c31ceecff51aec30b0e04c5e2548f2525a2fa75b
Ana Bia <123493745+beatrizldsantos@users.noreply.github.com>
2023-07-22 23:11:28 -0300

    Ajuste

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/XMLAccessor.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/XMLAccessor.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
