9d5440c671d77a4350df83f8cc362cf471146dba
Lucas Camilo <lucas.camilobezerra@upe.br>
2023-08-05 17:24:20 -0300

    Alteração nas classes SlideItem e SlideViewerComponent

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideItem.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideItem.java
@@ -29,8 +29,8 @@ public abstract class SlideItem {
 		return level;
 	}
 
-	public abstract Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style style);
+	public abstract Rectangle getBoundingBox(Graphics slideGraphics, ImageObserver observer, float scale, Style style);
 
-	public abstract void draw(int x, int y, float scale, Graphics g, Style style, ImageObserver observer);
+	public abstract void draw(int x, int y, float scale, Graphics slideGraphics, Style style, ImageObserver observer);
 
 }
--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerComponent.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/SlideViewerComponent.java
@@ -61,21 +61,21 @@ public class SlideViewerComponent extends JComponent implements Serializable {
 	}
 
 	@Override
-	public void paintComponent(Graphics g) {
-		g.setColor(BGCOLOR);
-		g.fillRect(0, 0, getSize().width, getSize().height);
+	public void paintComponent(Graphics slideGraphics) {
+		slideGraphics.setColor(BGCOLOR);
+		slideGraphics.fillRect(0, 0, getSize().width, getSize().height);
 
 		if (presentation.getSlideNumber() < 0 || slide == null) {
 			return;
 		}
 
-		g.setFont(labelFont);
-		g.setColor(COLOR);
-		g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), XPOS, YPOS);
+		slideGraphics.setFont(labelFont);
+		slideGraphics.setColor(COLOR);
+		slideGraphics.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), XPOS, YPOS);
 
 		Rectangle area = new Rectangle(0, YPOS, getWidth(), (getHeight() - YPOS));
 
-		slide.draw(g, area, this);
+		slide.draw(slideGraphics, area, this);
 	}
 
 }
