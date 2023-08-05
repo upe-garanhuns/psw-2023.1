a3b25602b6de2f5b238649687bfb7c63bef53ecc
Beatriz Leite dos Santos <beatriz.leitesantos@upe.br>
2023-08-05 13:35:51 -0300

    Atualizando as classes DemoPresentation e JabberPointApplication

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/view/Style.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/view/Style.java
@@ -1,10 +1,10 @@
 /**
  * UPE - Campus Garanhuns Curso de Bacharelado em Engenharia de Software
  * Disciplina de Projeto de Software - 2023.1
- * <p>
+ *<p>
  * Licensed under the Apache License, Version 2.0
  * https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * @author Ian F. Darwin, Helaine Lins
  */
 package br.upe.enenhariasoftware.psw.jabberpoint.view;
@@ -14,44 +14,58 @@ import java.awt.Font;
 
 public class Style {
 
-  private static Style[] styles;
+    private static Style[] styles;
 
-  private static final String FONTNAME = "Helvetica";
-  int indent;
-  Color color;
-  Font font;
-  int fontSize;
-  int leading;
+    private static final String FONTNAME = "Helvetica";
+    int indent;
+    Color color;
+    Font font;
+    int fontSize;
+    int leading;
 
-  public static void createStyles() {
-    styles = new Style[5];
-    styles[0] = new Style(0, Color.red, 48, 20);
-    styles[1] = new Style(20, Color.blue, 40, 10);
-    styles[2] = new Style(50, Color.black, 36, 10);
-    styles[3] = new Style(70, Color.black, 30, 10);
-    styles[4] = new Style(90, Color.black, 24, 10);
-  }
-
-  public static Style getStyle(int level) {
-    if (level >= styles.length) {
-      level = styles.length - 1;
+    public static void createStyles() {
+        styles = new Style[5];
+        styles[0] = new Style(0, Color.red, 48, 20); // nível 0
+        styles[1] = new Style(20, Color.blue, 40, 10); // nível 1
+        styles[2] = new Style(50, Color.black, 36, 10); // nível 2
+        styles[3] = new Style(70, Color.black, 30, 10); // nivel 3
+        styles[4] = new Style(90, Color.black, 24, 10); // nível 4
     }
 
-    return styles[level];
-  }
+    public static Style getStyle(int level) {
+        if (level >= styles.length) {
+            level = styles.length - 1;
+        }
 
-  public Style(int indent, Color color, int points, int leading) {
-    this.indent = indent;
-    this.color = color;
-    font = new Font(FONTNAME, Font.BOLD, fontSize = points);
-    this.leading = leading;
-  }
+        return styles[level];
+    }
 
-  public String toString() {
-    return "[" + indent + "," + color + "; " + fontSize + " on " + leading + "]";
-  }
+    public Style(int indent, Color color, int points, int leading) {
+        this.indent = indent;
+        this.color = color;
+        this.fontSize = points;
+        font = new Font(FONTNAME, Font.BOLD, fontSize);
+        this.leading = leading;
+    }
 
-  public Font getFont(float scale) {
-    return font.deriveFont(fontSize * scale);
-  }
+    public String toString() {
+        return "[" + indent + "," + color + "; " + fontSize + " on " + leading + "]";
+    }
+
+    public Font getFont(float scale) {
+        return font.deriveFont(fontSize * scale);
+    }
+
+    public int getIndent() {
+        return indent;
+    }
+
+    public int getLeading() {
+        return leading;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
 }
