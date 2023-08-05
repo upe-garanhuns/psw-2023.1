90fc4dc3165e963f38a25a6b9bc601663725a50a
Beatriz Leite dos Santos <beatriz.leitesantos@upe.br>
2023-08-05 15:41:40 -0300

    Ajuste

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/model/Presentation.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/model/Presentation.java
@@ -1,7 +1,7 @@
 /**
  * UPE - Campus Garanhuns Curso de Bacharelado em Engenharia de Software
  * Disciplina de Projeto de Software - 2023.1
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0
  * https://www.apache.org/licenses/LICENSE-2.0
  * 
