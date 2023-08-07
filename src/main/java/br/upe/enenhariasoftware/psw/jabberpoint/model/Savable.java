a71e6107cf769e0a574c6855542808bc2bbf1833
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2023-08-07 12:21:47 -0300

    Update Accessible / XMLAccessor / MenuController
    Reestruturação e renomeação da interface Accessible e correções nas classes XMLAccessor e MenuController

--- src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/model/Savable.java
+++ src/main/java/br/upe/enenhariasoftware/psw/jabberpoint/model/Savable.java
@@ -3,9 +3,7 @@ package br.upe.enenhariasoftware.psw.jabberpoint.model;
 
 import java.io.IOException;
 
-public interface Accessible {
-
-    public abstract void loadFile(Presentation presentation, String fileName) throws IOException;
+public interface Savable {
 
     public abstract void saveFile(Presentation presentation, String fileName) throws IOException;
 
